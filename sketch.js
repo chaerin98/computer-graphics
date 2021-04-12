2bfa9ba8e6f81f04701b1db6e7481fe7c921cef0
chaerin98 <47683552+chaerin98@users.noreply.github.com>
2021-04-12 22:31:44 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,78 +1,31 @@
-let myFont;
-let building_sl, rotate_sl;
-let CITY_SIZE_X = 700;
-let CITY_SIZE_Y = 1000;
-let BUILDING_MAX_SIZE = 45;
-let gridSz_x = CITY_SIZE_X / BUILDING_MAX_SIZE;
-let gridSz_y = CITY_SIZE_Y / BUILDING_MAX_SIZE;
-let map = [];
-function resetMap() {
-  for (let i = 0; i < gridSz_x; i++)
-    for (let j = 0; j < gridSz_y; j++) 
-      map[i][j] = false;
+let teapot;
+let rot;
+let slider;
+function preload() {
+teapot = loadModel('assets/teapot.obj');
 }
-
-function preload(){
-  myFont = loadFont('assets/STIXGeneral.otf');
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  colorMode(RGB, 1);
-  // init map
-  for (let i = 0; i < gridSz_x; i++) {
-    map[i] = [];
-    for (let j = 0; j < gridSz_y; j++) {
-      map[i][j] = false;
-    }
-  }
-  building_sl = createSlider(30, 250, 3);
-  building_sl.position(10, 30);
-  rotate_sl = createSlider(110, 180, 0);
-  rotate_sl.position(10, 50);
-
+createCanvas(windowWidth, windowHeight, WEBGL);
+colorMode(HSB, 360, 100, 100);
+slider = createSlider(0, 360, 120);
+slider.position(10, 10);
+slider.style('width', '80px');
+rot=0;
 }
 function draw() {
-  background('#001036');
-  randomSeed(0);
-  lights();
-  directionalLight(1, 0, 0, 1, 0, 0);
-  rotateX(radians(rotate_sl.value()));
-  fill(0.4);
-  plane(800, 1200); // draw ground
-  noStroke();
-  let nBuildings = building_sl.value();
-  translate(-CITY_SIZE_X / 2, -CITY_SIZE_Y / 2);
-  
-  for (let i = 0; i < nBuildings; i++) {
-    let foundEmptySpot = false;
-    let x = 0,
-    y = 0;
-    // loop until it finds an empty spot
-    while (foundEmptySpot == false) {
-      x = floor(random(0, gridSz_x));
-      y = floor(random(0, gridSz_y));
-      if (map[x][y] == false) {
-        foundEmptySpot = true;
-        map[x][y] = true;
-      }
-    }
-    // randomly determine building dimensions
-    let w = random(15, BUILDING_MAX_SIZE);
-    let h = random(15, BUILDING_MAX_SIZE);
-    let d = random(10, 100); // building height
-    // render a building
-    let r = random(1, 5);
-    if(r >= 1 && r<2)  fill('#F2F0CE');
-    else if(r >= 2 && r<3)  fill('#FAF566');
-    else if(r >= 3 && r<4)  fill('#A68524');
-    else  fill('#F2E205');
-    
-    push();
-    translate(x * BUILDING_MAX_SIZE, y * BUILDING_MAX_SIZE, d / 2);
-    box(w, h, d);
-    pop();
-  }
-  
-  resetMap();
+background(0);
+// setting lights
+pointLight(0, 0, 100, 0, 0, 500);
+// transform the teapot
+translate(0, 200, -300);
+rotateX(HALF_PI);
+rotateZ(rot);
+scale(20);
+noStroke();
+// set teapot material colors
+fill(slider.value(), 100, 100);
+// display teapots
+model(teapot);
+// rotate teapots
+rot += 0.02;
 }
